ff6c983f890cf572a91cae859125a40c4a210c9f
shreyash mukte <115740088+Shreyash642@users.noreply.github.com>
2023-03-18 04:02:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,36 @@
 import streamlit
 
-streamlit.title('My Parents Are New Healthy Diner')
-   
+
+
+
+streamlit.title('My Parents New Healthy Diner')
+
+
+
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocodo Toast')
+
+streamlit.text('🥑🍞 Avacado Toast')
+
+
+
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+
+
 import pandas
+
+
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -20,20 +41,29 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
+
+
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
 
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+
+
+
+
 
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
+
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
 
 
 
